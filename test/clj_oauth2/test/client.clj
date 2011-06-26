144120a1020794f717943cb4c0df911d3eca282f
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2011-06-27 01:51:03 +0200

    rudimentary make-request implementation

--- test/clj_oauth2/test/client.clj
+++ test/clj_oauth2/test/client.clj
@@ -1,6 +1,6 @@
 (ns clj-oauth2.test.client
-  (:use [clj-oauth2.client])
-  (:use [lazytest.describe]))
+  (:use [clj-oauth2.client]
+        [lazytest.describe]))
 
 (def endpoint
   {:authorization-uri "http://example.com/auth"
@@ -10,7 +10,6 @@
    :redirect-uri "http://my.host/cb"})
 
 (describe "grant-type authorization-code"
-  (given [req (make-request (assoc endpoint :grant-type 'authorization-code))
-          uri (parse-uri (:uri req))]
+  (given [req (make-request (assoc endpoint :grant-type 'authorization-code))]
     (it "constructs a uri for the authorization redirect"
-      (= (get-in uri [:query-params :response_type]) "code"))))
+      (= (:uri req) "http://example.com/auth?response_type=code"))))
