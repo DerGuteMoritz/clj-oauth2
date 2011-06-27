6c0f4cfbe7b25555a739ab91d9eb430a8c6f251d
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2011-06-27 02:13:01 +0200

    handle scope and state arguments

--- src/clj_oauth2/client.clj
+++ src/clj_oauth2/client.clj
@@ -3,9 +3,19 @@
         [clojure.contrib.json :only [read-json]]
         [clojure.contrib.java-utils]
         [clj-oauth2.uri])
-  (:require [clj-http.client :as http]))
+  (:require [clj-http.client :as http]
+            [clojure.string :as str]))
 
 (defn make-request [endpoint]
   (let [uri (parse-uri (:authorization-uri endpoint))
-        uri (assoc-in uri [:query :response_type] "code")]
-    {:uri (str (make-uri uri))}))
+        uri (assoc-in uri [:query :response_type] "code")
+        uri (if (:state endpoint)
+              (assoc-in uri [:query :state] (:state endpoint))
+              uri)
+        uri (if (:scope endpoint)
+              (assoc-in uri [:query :scope]
+                        (str/join " " (:scope endpoint)))
+              uri)]
+    {:uri (str (make-uri uri))
+     :scope (:scope endpoint)
+     :state (:state endpoint)}))
--- test/clj_oauth2/test/client.clj
+++ test/clj_oauth2/test/client.clj
@@ -1,5 +1,6 @@
 (ns clj-oauth2.test.client
   (:use [clj-oauth2.client]
+        [clj-oauth2.uri]
         [lazytest.describe]))
 
 (def endpoint
@@ -10,6 +11,16 @@
    :redirect-uri "http://my.host/cb"})
 
 (describe "grant-type authorization-code"
-  (given [req (make-request (assoc endpoint :grant-type 'authorization-code))]
+  (given [req (make-request (assoc endpoint
+                              :grant-type 'authorization-code
+                              :scope ["foo" "bar"]
+                              :state "bazqux"))
+          uri (parse-uri (:uri req))]
     (it "constructs a uri for the authorization redirect"
-      (= (:uri req) "http://example.com/auth?response_type=code"))))
+      (and (= (:scheme uri) "http")
+           (= (:host uri) "example.com")
+           (= (:path uri) "/auth")
+           (= (:query uri) {:response_type "code" :scope "foo bar" :state "bazqux"})))
+    (it "contains the passed in scope and state"
+      (and (= (:scope req) ["foo" "bar"])
+           (= (:state req) "bazqux")))))
