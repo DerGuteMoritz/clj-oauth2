e873c8e7d8ba49b9151d9c99464fc8962d44e53d
Robert Levy <r.p.levy@gmail.com>
2011-12-18 13:29:55 -0500

    update clj-http version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject rplevy/clj-oauth2 "0.1.0"
   :description "clj-http and ring middlewares for OAuth 2.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.1.3"]]
+                 [clj-http "0.2.6"]]
   :dev-dependencies [[ring "0.3.11"]
                      [com.stuartsierra/lazytest "1.1.2"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
