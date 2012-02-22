1719f4586b1cef4ba9c8b11a5ba80b1175eb304b
Manoj Waikar <mmwaikar@gmail.com>
2012-02-22 15:04:01 +0530

    Make it clojure 1.2.1 compatible.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-oauth2 "0.1.1"
   :description "clj-http and ring middlewares for OAuth 2.0"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/data.json "0.1.1"]
                  [clj-http "0.2.6"]]
   :dev-dependencies [[ring "0.3.11"]
