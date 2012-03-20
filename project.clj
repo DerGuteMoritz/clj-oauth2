033e1c16755b7d7bc8ceb2fc10177a352c67c792
Anthony Grimes <i@raynes.me>
2012-03-20 17:49:57 -0500

    Upgrade clj-http

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "clj-http and ring middlewares for OAuth 2.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
-                 [clj-http "0.2.6"]
+                 [clj-http "0.3.2"]
                  [uri "1.1.0"]
                  [commons-codec/commons-codec "1.6"]]
   :dev-dependencies [[ring "0.3.11"]]
